d28ccb5e38731826240e8db22f91402e26af528b
root <root@localhost.localdomain>
2022-09-11 23:35:33 +0800

    adding 2 file for learning PHP

---
+++ chapter01/processOrder.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bob's Auto Part - Order Results</title>
+</head>
+<body>
+    <h1>Bob's Auto Part</h1>
+    <h2>Order Results</h2>
+    <?php
+        echo '<p>Order Process.</p>';
+    ?>
+
+    
+</body>
+</html>
