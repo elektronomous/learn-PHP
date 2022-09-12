d4d60a1fb135ef17615acc9ce0797c99bf601ad8
Muhammad Faza Akbar <akbarfaza222@gmail.com>
2022-09-12 13:34:30 +0800

    editing the $totalqty

--- chapter01/processOrder.php
+++ chapter01/processOrder.php
@@ -8,12 +8,14 @@ $sparkQty = $_POST['sparkqty'];
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bob's Auto Part - Order Results</title>
 </head>
+
 <body>
     <h1>Bob's Auto Part</h1>
     <h2>Order Results</h2>
@@ -28,6 +30,7 @@ $sparkQty = $_POST['sparkqty'];
         
     ?>
 
-    
+
 </body>
+
 </html>
