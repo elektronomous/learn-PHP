5fdb9da87ae1c9d8ed9f48006f0f9e2f76f689a6
Muhammad Faza Akbar <akbarfaza222@gmail.com>
2022-09-12 13:52:52 +0800

    update the logic

--- chapter01/processOrder.php
+++ chapter01/processOrder.php
@@ -23,11 +23,33 @@ $sparkQty = $_POST['sparkqty'];
         echo '<p>Order Process at ';
         echo date('H:i jS F Y') . '</p>';
 
-        echo '<p>Your orders as follows: </p>';
-        echo htmlspecialchars($tireQty) . ' ' . $tireQty > 1 ? 'tires' : 'tire' . '<br />';
-        echo htmlspecialchars($oilQty) . ' ' . $oilQty > 1 ? 'tires' : 'tire' . '<br />';
-        echo htmlspecialchars($sparkQty) . ' ' . $sparkQty > 1 ? 'tires' : 'tire' . '<br />';
+        $totalQty = 0;
+
+        define('TIREPRICE', 100);
+        define('OILPRICE', 10);
+        define('SPARKPRICE', 4);
+
+        $taxRage = 0.10;        // local sales tax rate;
         
+        $totalQty += $tireQty + $oilQty + $sparkQty;
+
+        if(! $totalQty) {
+            echo 'You did not order anything on the previous page<br />';
+        } else {
+            echo '<p>Your orders as follows: </p>';
+            echo htmlspecialchars($tireQty) . ' ' . ($tireQty > 1 ? 'tires' : 'tire') . '<br />';
+            echo htmlspecialchars($oilQty) . ' ' . ($oilQty > 1 ? 'tires' : 'tire') . '<br />';
+            echo htmlspecialchars($sparkQty) . ' ' . ($sparkQty > 1 ? 'tires' : 'tire') . '<br />';
+
+            $totalAmount = TIREPRICE * $tireQty
+                            + SPARKPRICE * $sparkQty
+                            + OILPRICE * $oilQty;
+            
+            echo 'Subtotal: ' . number_format($totalQty, 2) . '<br />';
+            $totalAmount *= (1+$taxRage);
+
+            echo 'Total including tax: $' . number_format($totalAmount, 2) . '<br />';
+        }
     ?>
 
 
