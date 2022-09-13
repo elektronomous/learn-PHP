bb0a44ce0b5087335e863ab7971a7ca440432713
elektronomous <akbarfaza222@gmail.com>
2022-09-14 06:30:13 +0800

    testing using fgetscsv

--- chapter01/testing.php
+++ chapter01/testing.php
@@ -14,11 +14,14 @@ else{
 file_put_contents('test.bat','');
 */
 
-$fp = fopen('orders.txt','w');
+$fp = fopen('orders.txt','r');
 
 if(!$fp){
     echo 'couldn\'t open the file';
     return;
+} else {
+    var_dump(fgetcsv($fp,0,"\t","|"));
+    
 }
 
 fclose($fp);
