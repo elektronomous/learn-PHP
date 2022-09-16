4fcb369562c3fb92dbb92aeb369f668b3f6da7d1
Muhammad Faza Akbar <akbarfaza222@gmail.com>
2022-09-16 15:19:09 +0800

    using list to destruct the associative array

--- chapter03/array.php
+++ chapter03/array.php
@@ -6,6 +6,8 @@ require_once(__DIR__ . "/../Helper.php");
 $products = array('Tires', 'Oil', 'Spark plugs');
 // or like this
 $products = ['Tires', 'Oil', 'Spark Plugs'];
+list($product1, $product2) = $products;
+echo 'this first product ' . $product1 . '<br />';
 
 // you can create an array that contain with sequence number
 $seqNumber = range(0,10);
@@ -30,6 +32,10 @@ varDump($products);
 
 // now you can create an array using string indexes
 $products = array('Tires' => 100, 'Oil' => 10, 'Spark Plugs' => 14);
+list('Tires'=> $product1, 'Oil' => $product2) = $products;
+
+echo $product1 . ' and ' . $product2 . '<br />';
+
 
 // you access the associative array by index its string
 echo $products['Tires'] . '<br />';
@@ -37,6 +43,10 @@ echo $products['Tires'] . '<br />';
 // you can create a new element into the associative array
 $products['Fuses'] = 50;
 
+// now to be faster, you can use loop for access the array
+foreach($products as $key => $value) {
+    echo $key . " - " . $value . "<br />";
+}
 
 
 
