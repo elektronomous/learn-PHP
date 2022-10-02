2afcc20a97a2fe8d10e7442f1e8cefdbd08be930
elektronomous <akbarfaza222@gmail.com>
2022-10-02 18:40:51 +0800

    split home.html to php files

---
+++ chapter05/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>TLA Consulting Pty Ltd</title>
+    <link href="style.css" type="text/css" rel="stylesheet" />
+  </head>
+  <body>
+    <!-- page header -->
+    <header>
+      <img src="logo.gif" alt="TLA logo" height="70" width="70" />
+      <h1>TLA Consulting</h1>
+    </header>
+    <!-- page menu -->
+    <nav>
+      <div class="menuitem">
+        <a href="home.html">
+          <img src="s-logo.gif" alt="" height="20" width="20" />
+          <span class="menutext">Home</span>
+        </a>
+      </div>
+      <div class="menuitem">
+        <a href="contact.html">
+          <img src="s-logo.gif" alt="" height="20" width="20" />
+          <span class="menutext">Contact</span>
+        </a>
+      </div>
+      <div class="menuitem">
+        <a href="services.html">
+          <img src="s-logo.gif" alt="" height="20" width="20" />
+          <span class="menutext">Services</span>
+        </a>
+      </div>
+      <div class="menuitem">
+        <a href="about.html">
+          <img src="s-logo.gif" alt="" height="20" width="20" />
+          <span class="menutext">About</span>
+        </a>
+      </div>
+    </nav>
