bb228d1f8d0d0b5c5f840c6a4717a5fba80d29a1
elektronomous <akbarfaza222@gmail.com>
2022-11-13 11:35:17 +0800

    move the folder

--- LearningFromVideos/Core/www/Section08/index.php
+++ LearningFromVideos/Core/www/Section08/index.php
@@ -1,46 +1,42 @@
+<!-- 5 -->
 <?php
-declare(strict_types=1);
 
-// 21
-$myName = "Muhammad Faza Akbar";
-$hour = 12;
+// 14
+require_once "includes/database.php";
+// 8
+$sql = "SELECT * FROM articles";
+// 8 - send query
+$results = mysqli_query($connect, $sql);
 
-$fruits = ["Apple", "Bananas", "Strawberry"];
+// 9
+if ($results === false) {
+    echo mysqli_error($connect);
+} else {
+    $articles = mysqli_fetch_all($results, MYSQLI_ASSOC);
+/* 
+    echo "<pre>";
+    print_r($articles);
+    echo "<pre><br />"; 
+*/
+
+}
+
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>First Website</title>
-</head>
-
-<body>
-    <h1>First Website</h1>
-    <?php if($hour < 12): ?>
-    <h2>Good Morning <?php echo $myName; ?></h2>
-    <?php elseif ($hour < 18): ?>
-    <h2>Good Afternoon <?php echo $myName; ?></h2>
-    <?php elseif ($hour < 22): ?>
-    <h2>Good Evening <?php echo $myName; ?></h2>
-    <?php else: ?>
-    <h2>Good Night</h2>
-    <?php endif; ?>
-
-    <ol>
-        <?php foreach($fruits as $fruit): ?>
-        <li><?php echo $fruit; ?></li>
-        <?php endforeach; ?>
-    </ol>
-    <ol>
-        <?php for($i = 0; $i < count($fruits); $i++): ?>
-        <li><?php echo $fruit; ?></li>
-        <?php endfor; ?>
-    </ol>
-    <pre><?php print_r($_SERVER);?></pre>
-    <pre><?php print_r($_GET); ?></pre>
-</body>
-
-</html>
+<?php require_once "includes/header.php" ?>
+<?php if (empty($articles)): ?>
+<p>No articles found.</p>
+<?php else: ?>
+<ul>
+    <?php foreach($articles as $article): ?>
+    <li>
+        <article>
+            <h2><a href="article.php?id=<?= $article['id']?>"><?= $article['title'] ?></a></h2>
+            <p><?= $article['content'] ?> </p>
+        </article>
+    </li>
+    <?php endforeach; ?>
+</ul>
+<?php endif; ?>
+<?php require_once "includes/footer.php" ?>
